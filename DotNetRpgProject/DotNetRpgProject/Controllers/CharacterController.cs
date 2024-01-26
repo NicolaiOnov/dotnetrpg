3cf5652a9540ad5324ab4af81987ef90499b4d91
nv <kensaii999@gmail.com>
2024-01-26 18:08:25 +0200

    added char service and make controller methods async

---
+++ DotNetRpgProject/DotNetRpgProject/Controllers/CharacterController.cs
@@ -0,0 +1,37 @@
+﻿using DotNetRpgProject.Model;
+using DotNetRpgProject.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetRpgProject.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CharacterController : Controller
+    {
+        private readonly ICharacterService _characterService;
+
+        public CharacterController(ICharacterService characterService)
+        {
+            _characterService = characterService;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Character>> GetCharacter(int id)
+        {
+            return Ok(await _characterService.GetCharacter(id));
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Character>>> GetCharacters()
+        {
+            return Ok(await _characterService.GetAllCharacters());
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<Character>>> CreateCharacter(Character newCharacter)
+        {
+            await _characterService.AddCharacter(newCharacter);
+            return await _characterService.GetAllCharacters();
+        }
+    }
+}
---
+++ DotNetRpgProject/DotNetRpgProject/Services/CharacterService.cs
@@ -0,0 +1,29 @@
+﻿using DotNetRpgProject.Model;
+
+namespace DotNetRpgProject.Services
+{
+    public class CharacterService : ICharacterService
+    {
+        private static readonly List<Character> Characters = new()
+        {
+            new Character{Id = 0},
+            new Character{Id = 1, Name = "Sam"} 
+        };
+
+        public async Task<Character> GetCharacter(int id)
+        {
+            return Characters.SingleOrDefault(c => c.Id == id) 
+                   ?? throw new Exception("Character not found");
+        }
+
+        public async Task<List<Character>> GetAllCharacters()
+        {
+            return Characters;
+        }
+
+        public async Task AddCharacter(Character newCharacter)
+        {
+            Characters.Add(newCharacter);
+        }
+    }
+}
